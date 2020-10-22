38463de757c076839bae7967d6c0948db075f20e
bobbyseptianto <bobbyseptianto@gmail.com>
2020-10-22 22:23:45 +0700

    Fitur User

--- routers/user-router.js
+++ routers/user-router.js
@@ -9,6 +9,6 @@ router.post("/:id/profile/edit", UserController.editProfile)
 router.get("/:id/profile/delete", UserController.deleteProfileAccount)
 router.get("/:id/book-ticket", UserController.bookTicketForm)
 router.post("/:id/book-ticket", UserController.bookTicket)
-// router.get("/:id/my-ticket", UserController.showMyTicket)
+router.get("/:id/my-ticket", UserController.showMyTicket)
 
 module.exports = router;
